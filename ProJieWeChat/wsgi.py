d5449b74088fd5d79950c74b139e3ac9d9fe35cd
ProJie <1737723363@qq.com>
2017-02-01 14:25:57 +0800

    push

--- ProJieWeChat/wsgi.py
+++ ProJieWeChat/wsgi.py
@@ -12,11 +12,11 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 3
-
 import sys  # 4
 
 sys.path.insert(0, PROJECT_DIR)  # 5
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProJieWeChat.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProJieWeChat.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "ProJieWeChat.settings"
 
 application = get_wsgi_application()
